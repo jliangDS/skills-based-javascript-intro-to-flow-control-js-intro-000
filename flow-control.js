c940b7f1bc28b428dca23f28dc0ebda6083ad924
Jun Liang <illuminatemynight@gmail.com>
2019-06-16 17:19:20 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,7 +5,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if(age >=13 && age <=19){
+  if(age >= 13 && age <= 19){
     return "You are a teenager!";
   }
   else{
@@ -14,7 +14,15 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if(age >= 13 && age <= 19){
+    return "You are a teenager!";
+  }
+  else if(age <= 12){
+    return "You are a kid";
+  }
+  else {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
